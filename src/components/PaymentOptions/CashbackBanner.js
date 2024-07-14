92875d3943cd4e71d7b8b7627801d02421aae402
Fabio <fabiot92@gmail.com>
2024-07-13 23:16:43 -0300

    Update CashbackBanner.js

--- src/components/PaymentOptions/CashbackBanner.js
+++ src/components/PaymentOptions/CashbackBanner.js
@@ -3,7 +3,7 @@ import styles from "./CashbackBanner.module.css";
 
 const CashbackBanner = ({ boldText, normalText, className }) => {
   return (
-    <div className={`${className} ${styles.bannerContainer}`}>
+    <div className={styles.bannerContainer}>
       <div className={styles.bannerBackground}>
         <div className={styles.bannerContent}>
           <span className={styles.extraBoldText}>{boldText}</span>

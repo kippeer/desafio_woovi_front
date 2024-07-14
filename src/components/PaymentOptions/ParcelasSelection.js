45458529b9fffe5d27841babf503171616b9db9c
Fabio <fabiot92@gmail.com>
2024-07-14 10:32:55 -0300

    Ajuste de bug de voltar tela

--- src/components/PaymentOptions/ParcelasSelection.js
+++ src/components/PaymentOptions/ParcelasSelection.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { ValorParcelaDTO } from '../utils/dados/valorParcela.dto';
 import styles from "./ParcelasSelection.module.css";
 import { Checkbox } from "@mui/material";
@@ -22,9 +22,14 @@ const ParcelasSelection = () => {
 
   const confirmSelection = () => {
     setShowModal(false);
-    // Redirecionar para a página page1 com o ID da parcela
-    window.location.href = `/page1/${valorParcela.id}`;
   };
+
+  useEffect(() => {
+    if (valorParcela) {
+      // Redirecionar para a página page1 com o ID da parcela
+      window.location.href = `/page1/${valorParcela.id}`;
+    }
+  }, [valorParcela]);
 
   const cancelSelection = () => {
     setSelectedParcela(null);
@@ -33,86 +38,83 @@ const ParcelasSelection = () => {
 
   return (
     <>
-    
-    <div className={styles.parcelasContainer}>
-      {parcelas.map((parcela) => (
-        <div
-          key={parcela.id}
-          className={`${styles.parcela} ${
-            selectedParcela === parcela.id ? styles.selected : ""
-          }`}
-          onClick={() => handleParcelaClick(parcela)}
-        >
-          <div className={styles.empilhamento}>
-            <span className={styles.parcelaText}>
-              <span className={styles.parcelaIdText}>{parcela.id}x</span>
-              <span className={styles.parcelaAmountText}>
-                {" "}
-                {parcela.amount.toLocaleString("pt-BR", {
-                  style: "currency",
-                  currency: "BRL",
-                })}
-              </span>
-            </span>
-            {parcela.id === 1 && (
-              <>
-                <span className={styles.cashbackText}>
-                  Ganhe 3% de Cashback
+      <div className={styles.parcelasContainer}>
+        {parcelas.map((parcela) => (
+          <div
+            key={parcela.id}
+            className={`${styles.parcela} ${
+              selectedParcela === parcela.id ? styles.selected : ""
+            }`}
+            onClick={() => handleParcelaClick(parcela)}
+          >
+            <div className={styles.empilhamento}>
+              <span className={styles.parcelaText}>
+                <span className={styles.parcelaIdText}>{parcela.id}x</span>
+                <span className={styles.parcelaAmountText}>
+                  {" "}
+                  {parcela.amount.toLocaleString("pt-BR", {
+                    style: "currency",
+                    currency: "BRL",
+                  })}
                 </span>
-                <CashbackBanner
-                  boldText="🤑 R$ 300,00"
-                  normalText=" de volta no seu Pix na hora"
-                  
-                />
-              </>
-            )}
-            {parcela.id !== 1 && (
-              <span className={styles.totalText}>
-                Total:{" "}
-                {parcela.total.toLocaleString("pt-BR", {
-                  style: "currency",
-                  currency: "BRL",
-                })}
               </span>
-            )}
-            {parcela.id === 4 && (
-              <CashbackBanner
-                boldText="-3% de juros:"
-                normalText=" Melhor opção de parcelamento"
-                
+              {parcela.id === 1 && (
+                <>
+                  <span className={styles.cashbackText}>
+                    Ganhe 3% de Cashback
+                  </span>
+                  <CashbackBanner
+                    boldText="🤑 R$ 300,00"
+                    normalText=" de volta no seu Pix na hora"
+                  />
+                </>
+              )}
+              {parcela.id !== 1 && (
+                <span className={styles.totalText}>
+                  Total:{" "}
+                  {parcela.total.toLocaleString("pt-BR", {
+                    style: "currency",
+                    currency: "BRL",
+                  })}
+                </span>
+              )}
+              {parcela.id === 4 && (
+                <CashbackBanner
+                  boldText="-3% de juros:"
+                  normalText=" Melhor opção de parcelamento"
+                />
+              )}
+            </div>
+            <div className={styles.checkbox}>
+              <Checkbox
+                icon={<RadioButtonUncheckedIcon />}
+                checkedIcon={
+                  <CheckCircleIcon style={{ color: "#4caf50" }} />
+                }
+                checked={selectedParcela === parcela.id}
+                onChange={() => handleParcelaClick(parcela)}
               />
+            </div>
+            {parcela.id === 1 && (
+              <div className={styles.pixParcelado}>
+                Pix 
+              </div>
+            )}
+            {parcela.id === 2 && (
+              <div className={styles.pixParcelado}>
+                Pix Parcelado
+              </div>
             )}
           </div>
-          <div className={styles.checkbox}>
-            <Checkbox
-              icon={<RadioButtonUncheckedIcon />}
-              checkedIcon={
-                <CheckCircleIcon style={{ color: "#4caf50" }} />
-              }
-              checked={selectedParcela === parcela.id}
-              onChange={() => handleParcelaClick(parcela)}
-            />
-          </div>
-          {parcela.id === 1 && (
-            <div className={styles.pixParcelado}>
-              Pix 
-            </div>
-          )}
-          {parcela.id === 2 && (
-            <div className={styles.pixParcelado}>
-              Pix Parcelado
-            </div>
-          )}
-        </div>
-      ))}
-      {showModal && (
-        <Modal
-          confirmSelection={confirmSelection}
-          cancelSelection={cancelSelection}
-          valorParcela={valorParcela}
-        />
-      )}
-    </div>
+        ))}
+        {showModal && (
+          <Modal
+            confirmSelection={confirmSelection}
+            cancelSelection={cancelSelection}
+            valorParcela={valorParcela}
+          />
+        )}
+      </div>
     </>
   );
 };

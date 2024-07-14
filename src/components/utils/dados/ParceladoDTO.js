d314dfb3571e16df32e146f7ede3b922bcafa661
Fabio <fabiot92@gmail.com>
2024-07-13 21:06:12 -0300

    dados

---
+++ src/components/utils/dados/ParceladoDTO.js
@@ -0,0 +1,47 @@
+const ParceladoDTO = {
+    '1': [
+      { id: 1, texto: '1ª entrada no Pix', valor: 30500.0 }
+    ],
+    '2': [
+      { id: 2, texto: '1ª entrada no Pix', valor: 15300.0 },
+      { id: 2, texto: '2ª no cartão', valor: 15300.0 }
+    ],
+    '3': [
+      { id: 3, texto: '1ª entrada no Pix', valor: 10196.66 },
+      { id: 3, texto: '2ª no cartão', valor: 10196.66 },
+      { id: 3, texto: '3ª no cartão', valor: 10196.66 }
+    ],
+    '4': [
+      { id: 4, texto: '1ª entrada no Pix', valor: 7725.0 },
+      { id: 4, texto: '2ª no cartão', valor: 7725.0 },
+      { id: 4, texto: '3ª no cartão', valor: 7725.0 },
+      { id: 4, texto: '4ª no cartão', valor: 7725.0 }
+    ],
+    '5': [
+      { id: 5, texto: '1ª entrada no Pix', valor: 6300.0 },
+      { id: 5, texto: '2ª no cartão', valor: 6300.0 },
+      { id: 5, texto: '3ª no cartão', valor: 6300.0 },
+      { id: 5, texto: '4ª no cartão', valor: 6300.0 },
+      { id: 5, texto: '5ª no cartão', valor: 6300.0 }
+    ],
+    '6': [
+      { id: 6, texto: '1ª entrada no Pix', valor: 5283.33 },
+      { id: 6, texto: '2ª no cartão', valor: 5283.33 },
+      { id: 6, texto: '3ª no cartão', valor: 5283.33 },
+      { id: 6, texto: '4ª no cartão', valor: 5283.33 },
+      { id: 6, texto: '5ª no cartão', valor: 5283.33 },
+      { id: 6, texto: '6ª no cartão', valor: 5283.33 }
+    ],
+    '7': [
+      { id: 7, texto: '1ª entrada no Pix', valor: 4542.85 },
+      { id: 7, texto: '2ª no cartão', valor: 4542.85 },
+      { id: 7, texto: '3ª no cartão', valor: 4542.85 },
+      { id: 7, texto: '4ª no cartão', valor: 4542.85 },
+      { id: 7, texto: '5ª no cartão', valor: 4542.85 },
+      { id: 7, texto: '6ª no cartão', valor: 4542.85 },
+      { id: 7, texto: '7ª no cartão', valor: 4542.85 }
+    ]
+  };
+  
+  export default ParceladoDTO;
+  
---
+++ src/components/utils/dados/valorParcela.dto.js
@@ -0,0 +1,11 @@
+
+export const ValorParcelaDTO = {
+    '1': { id: 1, amount: 30500.0, total: 30500.0 },
+    '2': { id: 2, amount: 15300.0, total: 30600.0 },
+    '3': { id: 3, amount: 10196.66, total: 30620.0 },
+    '4': { id: 4, amount: 7725.0, total: 30900.0 },
+    '5': { id: 5, amount: 6300.0, total: 31500.0 },
+    '6': { id: 6, amount: 5283.33, total: 31699.98 },
+    '7': { id: 7, amount: 4542.85, total: 31800.0 },
+  };
+  

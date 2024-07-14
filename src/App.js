c80dde4346f4ba2c03ca48d9f8872d3c9647ecb7
Fabio <fabiot92@gmail.com>
2024-07-13 21:04:17 -0300

    init

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Page2 from './pages/Page2';
+import PaymentScreen from './components/PaymentScreen/PaymentScreen';
+import Fim from './pages/Fim';
+const App = () => (
+  <Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/page1/:valorParcelaId" element={<PaymentScreen />} />
+      <Route path="/page2" element={<Page2 />} />
+      <Route path="/fim" element={<Fim />} />
+    </Routes>
+  </Router>
+);
+
+export default App;
